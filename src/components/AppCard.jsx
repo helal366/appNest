94b30554967ece1fe95b0ed54fdb00a07dccc8b4
Helal366 <helal56876@gmail.com>
2026-03-19 20:26:17 +0600

    aos and tailwind conflict cleared

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -6,23 +6,24 @@ const AppCard = ({ app }) => {
     const { downloads, image, ratingAvg, title, id } = app;
     return (
         <Link to={`/app_details/${id}`}>
-            <div data-aos="zoom-in"
-            className="card bg-white shadow-sm px-5 pt-5 rounded hover:scale-102 cursor-pointer h-full transition-all duration-200">
-                <figure className='bg-[#f1f1f1] '>
-                    <img className='rounded object-cover object-top h-64'
-                        src={image}
-                        alt={title} />
-                </figure>
-                <div>
-                    <p className='font-bold my-2 truncate text-center'>{title}</p>
-                    <div className='flex justify-between gap-6 flex-wrap mb-2 font-bold'>
-                        <div className='flex gap-2 text-green-500 bg-gray-200/90 rounded justify-center mb-1 items-center badge'>
-                            <span><Download size={16} stroke='currentColor' /></span>
-                            <span>{new Intl.NumberFormat("en-US", { notation: "compact" }).format(downloads)}</span>
-                        </div>
-                        <div className='flex gap-2 text-yellow-500 bg-gray-200/90 rounded justify-center mb-2 items-center badge'>
-                            <span><Star size={16} stroke='currentColor' fill='currentColor' /></span>
-                            <span>{ratingAvg}</span>
+            <div data-aos="zoom-in">
+                <div className="card bg-white shadow-sm px-5 pt-5 rounded hover:scale-105 cursor-pointer h-full transition-transform duration-500">
+                    <figure className='bg-[#f1f1f1] '>
+                        <img className='rounded object-cover object-top h-64'
+                            src={image}
+                            alt={title} />
+                    </figure>
+                    <div>
+                        <p className='font-bold my-2 truncate text-center'>{title}</p>
+                        <div className='flex justify-between gap-6 flex-wrap mb-2 font-bold'>
+                            <div className='flex gap-2 text-green-500 bg-gray-200/90 rounded justify-center mb-1 items-center badge'>
+                                <span><Download size={16} stroke='currentColor' /></span>
+                                <span>{new Intl.NumberFormat("en-US", { notation: "compact" }).format(downloads)}</span>
+                            </div>
+                            <div className='flex gap-2 text-yellow-500 bg-gray-200/90 rounded justify-center mb-2 items-center badge'>
+                                <span><Star size={16} stroke='currentColor' fill='currentColor' /></span>
+                                <span>{ratingAvg}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
